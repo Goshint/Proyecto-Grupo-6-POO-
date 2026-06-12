aa56d9c5209de3e0ffb05cfe07cd23c416f29eb3
Goshint <joseph.janampa.p@uni.pe>
2026-06-11 21:32:44 -0500

    commit spring 2

---
+++ gimnasioVida_Fit/src/main/java/pe/edu/uni/gimnasioVida_Fit/GimnasioVidaFitApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.gimnasioVida_Fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioVidaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasioVidaFitApplication.class, args);
+	}
+
+}
---
+++ gimnasioVida_Fit/src/test/java/pe/edu/uni/gimnasioVida_Fit/GimnasioVidaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.uni.gimnasioVida_Fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioVidaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

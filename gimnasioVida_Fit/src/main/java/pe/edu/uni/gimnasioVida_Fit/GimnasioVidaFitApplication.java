54e28f2fb65e98186e3a510b09d09447a14ec035
Goshint <joseph.janampa.p@uni.pe>
2026-06-11 21:44:00 -0500

    commit spring 3

--- gimnasioVida_Fit/src/main/java/pe/edu/uni/gimnasioVida_Fit/GimnasioVidaFitApplication.java
+++ gimnasioVida_Fit/src/main/java/pe/edu/uni/gimnasioVida_Fit/GimnasioVidaFitApplication.java
@@ -1,6 +1,6 @@
 package pe.edu.uni.gimnasioVida_Fit;
 
-import org.springframework.boot.SpringApplication;
+import 	org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
